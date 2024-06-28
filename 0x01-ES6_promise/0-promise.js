84e9c79a4d2f19343355e264ef9e8811b979dd54
sarvie24 <samuelnnah080@gmail.com>
2024-06-28 15:06:52 -0700

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Response from API");
-        }, 1000);
-    });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
